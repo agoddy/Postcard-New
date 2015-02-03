db73ea47d2037f892572d0c5b5d16bfa2258300f
Sege_Oba <sege_oba@Olus-MacBook-Pro.local>
2015-02-02 20:38:06 -0600

    Name Label Functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
+    
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var txtEnterName: UITextField!
+    
+    @IBOutlet weak var txtEnterMessage: UITextField!
+    
+    @IBOutlet weak var btnSendMessage: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnSendMessage(sender: UIButton) {
+        lblMessage.text = "Message Sent"
+        lblMessage.hidden = false;
+        lblName.hidden = false;
+        lblName.text = "Hello " + txtEnterName.text;
+        lblName.textColor = UIColor.blueColor();
+        txtEnterName.text = ""
+        txtEnterName.resignFirstResponder();
+        txtEnterMessage.text = "";
+        txtEnterMessage.resignFirstResponder();
+        lblMessage.textColor = UIColor.redColor();
+        btnSendMessage.setTitle("Mail Sent", forState:UIControlState.Normal)
+        btnSendMessage.enabled = false
+    }
 
 }
 
